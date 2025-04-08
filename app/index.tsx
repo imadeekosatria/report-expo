d97e790a3d3f70af759944912214a2c2d3e95349
imadeekosatria <imadeekosatriawiguna@gmail.com>
2025-04-08 21:49:13 +0800

    feat: add login functionality with username and password inputs

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,25 @@
-import { View, Image, TextInput } from "react-native"
+import { useState } from "react";
+import { View, Image, TextInput, TouchableOpacity } from "react-native"
 
 const share = require("@/assets/images/share.png")
 
 export default function Index() {
+  const [username, onChangeUsername] = useState<string>()
+  const [password, onChangePassword] = useState<string>()
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Image
         source={share}
-        className="w-60 h-60"
+        className="w-60 h-60 my-8"
         alt="Logo"
       />
-      <TextInput />
+      <View className="flex flex-col items-center justify-center w-full gap-y-4">
+        <TextInput value={username} placeholder="username"  onChangeText={onChangeUsername} className="border rounded w-11/12 border-purple-600 border-collapse" />
+        <TextInput value={password} secureTextEntry placeholder="password" onChangeText={onChangePassword} className="border rounded w-11/12 border-purple-600 border-collapse" />
+        <TouchableOpacity onPress={()=>{console.log(username, password)}} className="bg-purple-600 rounded w-11/12 h-10 items-center justify-center">
+          <TextInput className="text-white">Login</TextInput>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
